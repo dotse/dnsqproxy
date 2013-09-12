cb4c3f5246e0d8c1062032c0756f26307c0e9913
Calle Dybedahl <calle@init.se>
2013-09-12 12:17:53 +0200

    Comments and documentation.

--- qproxy.pl
+++ qproxy.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright (c) 2013 Kirei AB. All rights reserved.
+# Copyright (c) 2013 Kirei AB, IIS. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -44,6 +44,8 @@ sub main {
         chomp;
         exit( 0 ) if ( $_ eq "" );
 
+        # Parse the incoming query request and exit the entire process
+        # if any single line cannot be parsed
         my $json_query = undef;
         eval { $json_query = from_json( $_ ); };
         if ( $@ ) {
@@ -52,9 +54,12 @@ sub main {
 
         my $resolver = setup_resolver( $json_query );
 
+        # Create a query object, for unclear reasons
         ## no critic (Modules::RequireExplicitInclusion)
         my $dns_query = Net::DNS::Packet->new( $json_query->{qname}, $json_query->{qtype}, $json_query->{qclass} );
 
+        # Measure time passed in the more cumbersome way that's useful
+        # if you work with picoseconds
         my $t1           = [gettimeofday];
         my $dns_response = $resolver->send( $dns_query );
         my $t2           = [gettimeofday];
@@ -68,11 +73,15 @@ sub main {
             'version'   => $version,
         };
 
-        # set tag in response if given in query
+        # set tag in response if given in query.
+
+        # This functionality is not (to my knowledge) documented, and
+        # certainly not used by the PDT testing code.
         if ( $json_query->{tag} ) {
             $json_response->{tag} = $json_query->{tag};
         }
 
+        # This must be a bug. The inner "else" branch can never be executed.
         if ( $dns_response ) {
             if ( $dns_response ) {
                 $json_response->{'response'} = encode_base64( $dns_response->data, "" );
@@ -99,6 +108,8 @@ sub fatal {
         'version' => $version,
     };
 
+    # Could have used a clearer say() here, since defined-or is used
+    # further down and also requires perl 5.10 or later
     print to_json( $json_response, { utf8 => 1 } ), "\n";
 
     exit( 0 );
@@ -137,11 +148,13 @@ sub setup_resolver {
       unless ( lc( $param->{transport} ) eq "tcp"
         or lc( $param->{transport} ) eq "udp" );
 
+    # Why disallow TCP timeout of more than 60s?
     fatal( "Invalid TCP timeout" )
       unless ( $param->{tcp_timeout} =~ /^\d+$/
         and $param->{tcp_timeout} > 0
         and $param->{tcp_timeout} <= 60 );
 
+    # Why disallow UDP timeout of more than 60s?
     if ( $param->{udp_timeout} ) {
         fatal( "Invalid UDP timeout" )
           unless ( $param->{udp_timeout} =~ /^\d+$/
@@ -162,7 +175,7 @@ sub setup_resolver {
     fatal( "Invalid UDP buffer size" )
       unless ( $param->{bufsize} =~ /^\d+$/
         and $param->{bufsize} > 0
-        and $param->{bufsize} <= 65536 );
+        and $param->{bufsize} <= 65536 ); # Bug, should be strictly less than 65536.
 
     # Validate flags
     fatal( "Failed to parse CD flag" ) unless is_boolean( $param->{flags}->{cd} );
@@ -182,9 +195,22 @@ sub setup_resolver {
     $res->cdflag( $param->{flags}->{cd} );
     $res->retrans( $param->{retrans} );    # retransmission interval
     $res->retry( $param->{retry} );        # query retries
+
+    # The following two lines are pointless, since queries are sent
+    # using the send() method, which ignores the search path and
+    # default names.
     $res->dnsrch( 0 );                     # do not use DNS search path
     $res->defnames( 0 );                   # no default names
+
+    # Why is this flag not settable by the user?
     $res->igntc( 1 );                      # ignore TC
+
+    # Why this weird restriction on setting the UDP packet size? It
+    # can be desirable even without DNSSEC (and contrary to the
+    # original comment below, setting the size does *not* necessarily
+    # require EDNS0), and setting the UDP packet size while using TCP
+    # is a noop, so why forbid it? Also, why just silently discard the
+    # request rather than give an error message?
 
     # set EDNS0 buffer size only if DO=1 and TCP is not used
     if ( $res->dnssec and not $res->usevc ) {
@@ -204,6 +230,10 @@ sub is_port {
     return ( $port > 0 or $port < 65536 );
 }
 
+# This function is completely broken. Due to the numeric comparison,
+# it will return true for anything that Perl numifies to zero. So
+# according to this, the strings "foobar" and "1life" are booleans,
+# while the string "5byfive" and the number 17 are not.
 sub is_boolean {
     my $x = shift;
 
@@ -238,6 +268,74 @@ The output DNS answer comes out on STDOUT, one JSON blob per line.
 A typical DNS query line looks like this:
 
 {"tcp_timeout":10,"transport":"udp","flags":{"cd":0,"ad":0,"do":0,"rd":1},"port":53,"qtype":"SOA","qclass":"IN","bufsize":1024,"qname":"kirei.se","address":"8.8.8.8","udp_timeout":10}
+
+If there is a parameter named C<tag>, it and its value will be copied to the response JSON string.
+
+=head2 Restrictions
+
+The script will print an error message and immediately exit if any of
+the following are true for any input line.
+
+=over
+
+=item *
+
+The incoming JSON string could not be parsed.
+
+=item *
+
+The C<address> key is unset or set to a false value.
+
+=item *
+
+The C<qname> key is unset or set to a false value.
+
+=item *
+
+The C<qtype> key is unset or set to a false value.
+
+=item *
+
+The C<address> parameter could not be parsed as an IPv4 or IPv6 address.
+
+=item *
+
+The C<port> parameter was not an integer between 1 and 65535.
+
+=item *
+
+The C<transport> parameter was not one of the two strings "tcp" or "udp" (case insensitive).
+
+=item *
+
+The C<tcp_timeout> parameter was not an integer between 1 and 60.
+
+=item *
+
+The C<udp_timeout> parameter was not an integer between 1 and 60.
+
+=item *
+
+The C<retrans> parameter was not an integer between 1 and 60.
+
+=item *
+
+The C<retry> parameter was not an integer between 0 and 10.
+
+=item *
+
+The C<bufsize> parameter was not an integer between 1 and 65536.
+
+=item *
+
+One or more of the flags C<cd>, C<rd>, C<ad> and C<do> held a value that perl converted to a number that was not 1 or 0.
+
+=back
+
+The C<bufsize> parameter is ignored unless the C<do> flag is true and
+the C<transport> parameter is C<"udp">.
+
+Any incoming DNS response packets with the truncation flag set will be ignored.
 
 =head1 JSON Answer Format
 
